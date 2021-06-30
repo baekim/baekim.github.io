4ec1755a5c64e3ceeba8b44170b516ec8bfa3033
benk <baemae.k@gmail.com>
2021-07-01 00:45:06 +0900

    분할된 포스트 목록을 받아 출력하기

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,7 +2,9 @@ import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import PostItem from 'components/Main/PostItem';
 import { FluidObject } from 'gatsby-image';
-import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import useInfiniteScroll, {
+    useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll';
 
 export type PostType = {
     node: {
@@ -45,11 +47,14 @@ const PostList: FunctionComponent<PostListProps> = function ({
     selectedCategory,
     posts,
 }) {
-    const { containerRef } = useInfiniteScroll(selectedCategory, posts);
+    const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
+        selectedCategory,
+        posts,
+    );
 
     return (
         <PostListWrapper ref={containerRef}>
-            {posts.map(({ node: { id, frontmatter } }: PostType) => (
+            {postList.map(({ node: { id, frontmatter } }: PostType) => (
                 <PostItem
                     {...frontmatter}
                     link="<https://www.google.co.kr/>"
--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,15 +1,36 @@
-import { MutableRefObject, useRef } from 'react';
+import { MutableRefObject, useState, useRef, useMemo } from 'react';
 import { PostType } from 'components/Main/PostList';
+
+export type useInfiniteScrollType = {
+    containerRef: MutableRefObject<HTMLDivElement | null>;
+    postList: PostType[];
+};
+
+const NUMBER_OF_ITEMS_PER_PAGE = 10;
 
 const useInfiniteScroll = function (
     selectedCategory: string,
     posts: PostType[],
-) {
+): useInfiniteScrollType {
     const containerRef: MutableRefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(
         null,
     );
+    const [count, setCount] = useState<number>(1);
 
-    return { containerRef };
+    const postListByCategory = useMemo<PostType[]>(
+        () =>
+            posts.filter(({ node: { frontmatter: { categories } } }: PostType) =>
+                selectedCategory !== 'All'
+                    ? categories.includes(selectedCategory)
+                    : true,
+            ),
+        [selectedCategory],
+    );
+
+    return {
+        containerRef,
+        postList: postListByCategory.slice(0, count * NUMBER_OF_ITEMS_PER_PAGE),
+    };
 };
 
 export default useInfiniteScroll;
