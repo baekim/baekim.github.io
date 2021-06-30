5faf848ce021db00b36a6479ef64c7563f67dcc1
benk <baemae.k@gmail.com>
2021-06-30 22:13:31 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,84 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import GlobalStyle from 'components/Common/GlobalStyle';
+import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+import PostList from 'components/Main/PostList';
+import { graphql } from 'gatsby';
+
+interface IndexPageProps {
+    data: {
+        allMarkdownRemark: {
+            edges: [
+                {
+                    node: {
+                        id: string;
+                        frontmatter: {
+                            title: string;
+                            summary: string;
+                            date: string;
+                            categories: string[];
+                            thumbnail: {
+                                publicURL: string;
+                            };
+                        };
+                    };
+                },
+            ];
+        };
+    };
+}
+
+const CATEGORY_LIST = {
+    All: 5,
+    Web: 3,
+    Mobile: 2,
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+    data: {
+        allMarkdownRemark: { edges },
+    },
+}) {
+    return (
+        <Container>
+            <GlobalStyle />
+            <Introduction />
+            <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+            <PostList />
+            <Footer />
+        </Container>
+    );
+};
+
+export default IndexPage;
+
+export const queryPostList = graphql`
+  query queryPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`;
