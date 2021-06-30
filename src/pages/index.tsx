e7722c94a70c085e032e119c7232c45406128134
benk <baemae.k@gmail.com>
2021-07-01 00:56:44 +0900

    메인 페이지 컴포넌트에 변경 사항 반영하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,11 @@
 import React, { FunctionComponent, useMemo } from 'react';
-import styled from '@emotion/styled';
-import GlobalStyle from 'components/Common/GlobalStyle';
-import Footer from 'components/Common/Footer';
+import Template from 'components/Common/Template';
 import CategoryList, { CategoryListProps } from 'components/Main/CategoryList';
 import Introduction from 'components/Main/Introduction';
 import PostList, { PostType } from 'components/Main/PostList';
 import { ProfileImageProps } from 'components/Main/ProfileImage';
 import { graphql } from 'gatsby';
 import queryString, { ParsedQuery } from 'query-string';
-
-// interface IndexPageProps {
-//     data: {
-//         allMarkdownRemark: {
-//             edges: PostType[];
-//         };
-//     };
-// }
-
-// interface IndexPageProps {
-//     data: {
-//         allMarkdownRemark: {
-//             edges: PostType[];
-//         };
-//         file: {
-//             childImageSharp: {
-//                 fluid: ProfileImageProps['profileImage'];
-//             };
-//         };
-//     };
-// }
 
 interface IndexPageProps {
     location: {
@@ -57,56 +34,6 @@ const CATEGORY_LIST = {
     Web: 3,
     Mobile: 2,
 };
-
-// const IndexPage: FunctionComponent<IndexPageProps> = function ({
-//     data: {
-//         allMarkdownRemark: { edges },
-//     },
-// }) {
-//     return (
-//         <Container>
-//             <GlobalStyle />
-//             <Introduction />
-//             <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-//             <PostList posts={edges} />
-//             <Footer />
-//         </Container>
-//     );
-// };
-
-// export default IndexPage;
-
-// export const queryPostList = graphql`
-//   query queryPostList {
-//     allMarkdownRemark(
-//       sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
-//     ) {
-//       edges {
-//         node {
-//           id
-//           frontmatter {
-//             title
-//             summary
-//             date(formatString: "YYYY.MM.DD.")
-//             categories
-//             thumbnail {
-//               childImageSharp {
-//                 fluid(
-//                   maxWidth: 768
-//                   maxHeight: 200
-//                   fit: INSIDE
-//                   quality: 100
-//                 ) {
-//                   ...GatsbyImageSharpFluid_withWebp
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
 
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
     location: { search },
