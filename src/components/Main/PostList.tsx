8d0ead3b1b1c97be63a6b803328a1c503cd89787
benk <baemae.k@gmail.com>
2021-08-04 01:06:42 +0900

    blog layout updated

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -3,37 +3,37 @@ import styled from '@emotion/styled';
 import PostItem from 'components/Main/PostItem';
 import { FluidObject } from 'gatsby-image';
 import useInfiniteScroll, {
-    useInfiniteScrollType,
+  useInfiniteScrollType,
 } from 'hooks/useInfiniteScroll';
 
 export type PostType = {
-    node: {
-        id: string;
-        fields: {
-            slug: string;
-        };
-        frontmatter: {
-            title: string;
-            summary: string;
-            date: string;
-            categories: string[];
-            thumbnail: {
-                childImageSharp: {
-                    fluid: FluidObject;
-                };
-            };
-        };
+  node: {
+    id: string;
+    fields: {
+      slug: string;
     };
+    frontmatter: {
+      title: string;
+      summary: string;
+      date: string;
+      categories: string[];
+      thumbnail: {
+        childImageSharp: {
+          fluid: FluidObject;
+        };
+      };
+    };
+  };
 };
 
 interface PostListProps {
-    selectedCategory: string;
-    posts: PostType[];
+  selectedCategory: string;
+  posts: PostType[];
 }
 
 const PostListWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  // grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
   width: 768px;
   margin: 0 auto;
@@ -47,29 +47,29 @@ const PostListWrapper = styled.div`
 `;
 
 const PostList: FunctionComponent<PostListProps> = function ({
+  selectedCategory,
+  posts,
+}) {
+  const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
     selectedCategory,
     posts,
-}) {
-    const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
-        selectedCategory,
-        posts,
-    );
+  );
 
-    return (
-        <PostListWrapper ref={containerRef}>
-            {postList.map(
-                ({
-                    node: {
-                        id,
-                        fields: { slug },
-                        frontmatter,
-                    },
-                }: PostType) => (
-                    <PostItem {...frontmatter} link={slug} key={id} />
-                ),
-            )}
-        </PostListWrapper>
-    );
+  return (
+    <PostListWrapper ref={containerRef}>
+      {postList.map(
+        ({
+          node: {
+            id,
+            fields: { slug },
+            frontmatter,
+          },
+        }: PostType) => (
+          <PostItem {...frontmatter} link={slug} key={id} />
+        ),
+      )}
+    </PostListWrapper>
+  );
 };
 
 export default PostList;
